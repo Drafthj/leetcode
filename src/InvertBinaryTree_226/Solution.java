eee2aa75ad2905647def152d944253fe12bdc462
drafthj <ahuojiaa@qq.com>
2016-11-28 15:36:44 +0800

    InvertBinaryTree_226

--- src/InvertBinaryTree_226/Solution.java
+++ src/InvertBinaryTree_226/Solution.java
@@ -1,5 +1,8 @@
 package InvertBinaryTree_226;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * Created by drafthj on 2016/11/28 14:40.
  */
@@ -20,6 +23,23 @@ public class Solution {
             root.right = t;
             invertTree(root.left);
             invertTree(root.right);
+        }
+        return root;
+    }
+    public TreeNode invertTree2(TreeNode root){
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()){
+            TreeNode treeNode = queue.poll();
+            TreeNode t = treeNode.left;
+            treeNode.left = treeNode.right;
+            treeNode.right = t;
+            if(treeNode.right!=null){
+                queue.add(treeNode.right);
+            }
+            if(treeNode.left!=null){
+                queue.add(treeNode.left);
+            }
         }
         return root;
     }
